8f16e5097042b6b7b866f7a0475dc84057aa6541
dhanjiRajput <kidechadhanji@gmail.com>
2025-04-03 16:11:54 +0530

    updated

--- GrapgQl/client/src/index.js
+++ GrapgQl/client/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache,ApolloProvider } from '@apollo/client';
 
-
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
   cache: new InMemoryCache(),
